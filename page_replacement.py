ec71268d8a09cce1d3c405f130fd0c648d0ac38b
Zephyr <zephyrxvxx7@gmail.com>
2019-10-31 16:57:03 +0800

    New feature: implement QQ Algorithm.

--- page_replacement.py
+++ page_replacement.py
@@ -1,10 +1,6 @@
 import random
-#import matplotlib.pyplot as plt
-from queue import Queue
 from collections import deque
 import time
-
-#plt.style.use('ggplot')
 
 MAX_STRING = 500
 MEMORY_REFERENCE_TIMES = 100000
@@ -14,6 +10,7 @@ FRAME_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 def random_reference():
     return [random.randint(1, MAX_STRING) for _ in range(MEMORY_REFERENCE_TIMES)]
 
+
 def locality_reference():
     locality_table = list()
 
@@ -22,11 +19,26 @@ def locality_reference():
         interval_loc = random.randint(1, MAX_STRING)
 
         if interval_loc + interval_range <= MAX_STRING:
-            locality_table.extend([random.randint(interval_loc, interval_loc + interval_range) for _ in range(random.randint(500, 1000))])
+            locality_table.extend([random.randint(
+                interval_loc, interval_loc + interval_range) for _ in range(random.randint(500, 1000))])
         else:
-            locality_table.extend([random.randint(interval_loc - interval_range, interval_loc) for _ in range(random.randint(500, 1000))])
+            locality_table.extend([random.randint(
+                interval_loc - interval_range, interval_loc) for _ in range(random.randint(500, 1000))])
 
     return locality_table[:MEMORY_REFERENCE_TIMES]
+
+
+def random_noramlvariate():
+    noramlvariate_table = list()
+
+    for _ in range(MEMORY_REFERENCE_TIMES):
+        tmp = 0
+        while(tmp < 1 or tmp > MAX_STRING):
+            tmp = int(random.normalvariate(int(MAX_STRING / 2), int(MAX_STRING / 5)))
+        noramlvariate_table.append(tmp)
+    
+    return noramlvariate_table
+
 
 def random_dirty_bits():
     return [random.randint(0, 1) for _ in range(MEMORY_REFERENCE_TIMES)]
@@ -37,24 +49,35 @@ def FIFO(process_table, dirty_bits, frame_size):
     interrupt = 0
     write_back = 0
 
-    frame_queue = Queue(maxsize=frame_size)
+    frame_queue = deque(maxlen=frame_size)
 
     for frame_iter, frame_id in enumerate(process_table):
-        if frame_id in frame_queue.queue:
+        hit_flag = False
+        for frame_dict in frame_queue:
+            if frame_id == frame_dict['id']:
+                temp_dict = frame_dict
+                if dirty_bits[frame_iter]:
+                    temp_dict['mod'] = dirty_bits[frame_iter]
+                frame_queue[frame_queue.index(frame_dict)] = temp_dict
+                hit_flag = True
+                break
+
+        if hit_flag:
             continue
-        
-        if frame_queue.full():
-            frame_queue.get()
-        
-        frame_queue.put(frame_id)
+
+        if len(frame_queue) == frame_size:
+            if frame_queue.popleft()['mod']:
+                write_back += 1
+                interrupt += 1
+
+        temp_dict = dict()
+        temp_dict['id'] = frame_id
+        temp_dict['mod'] = dirty_bits[frame_iter]
+        frame_queue.append(temp_dict)
 
         page_fault += 1
         interrupt += 1
 
-        if dirty_bits[frame_iter] == 1:
-            write_back += 1
-            interrupt += 1
-    
     return page_fault, interrupt, write_back
 
 
@@ -81,16 +104,15 @@ def Optimal(process_table, dirty_bits, frame_size):
 
                 if counter == frame_size - 1:
                     break
-            
+
             for index, _ in enumerate(victim_list):
                 if victim_list[index] == 0:
                     victim_list[index] = MEMORY_REFERENCE_TIMES
-            
+
             victim_index = victim_list.index(max(victim_list))
             frame_list[victim_index] = frame_id
         else:
             frame_list.append(frame_id)
-        
 
         page_fault += 1
         interrupt += 1
@@ -107,7 +129,7 @@ def enhance_second_chance(process_table, dirty_bits, frame_size):
 
     def generate_dict(id, ref_bit, mod_bit):
         return({'id': id, 'ref': ref_bit, 'mod': mod_bit})
-    
+
     def search_victim(frame_queue, frame_id, dirty_bit):
         for _ in range(0, 2):
             # Step a
@@ -144,10 +166,9 @@ def enhance_second_chance(process_table, dirty_bits, frame_size):
             if frame_id == frame_dict['id']:
                 hit_flag = True
                 break
-        
+
         if hit_flag:
             continue
-        
 
         if len(frame_queue) == frame_size:
 
@@ -156,28 +177,96 @@ def enhance_second_chance(process_table, dirty_bits, frame_size):
             if write_flag:
                 write_back += 1
                 interrupt += 1
-        else:
-            frame_queue.append(generate_dict(frame_id, 1, dirty_bits[frame_iter]))
-        
+
+        frame_queue.append(generate_dict(frame_id, 1, dirty_bits[frame_iter]))
+
         page_fault += 1
         interrupt += 1
-        
-    
+
+    return page_fault, interrupt, write_back
+
+
+def enhance_FIFO(process_table, dirty_bits, frame_size):
+    page_fault = 0
+    interrupt = 0
+    write_back = 0
+
+    frame_queue = deque(maxlen=frame_size)
+
+    for frame_iter, frame_id in enumerate(process_table):
+        if frame_id in frame_queue:
+            continue
+
+        if len(frame_queue) == frame_size:
+            if abs(frame_id - frame_queue[-1]) > abs(frame_id - frame_queue[0]):
+                frame_queue.pop()
+            else:
+                frame_queue.popleft()
+        else:
+            frame_queue.append(frame_id)
+
+        page_fault += 1
+        interrupt += 1
+
+        if dirty_bits[frame_iter] == 1:
+            write_back += 1
+            interrupt += 1
+
+    return page_fault, interrupt, write_back
+
+
+def QQ(process_table, dirty_bits, frame_size):
+    page_fault = 0
+    interrupt = 0
+    write_back = 0
+
+    frame_queue = deque(maxlen=frame_size)
+
+    for frame_iter, frame_id in enumerate(process_table):
+        hit_flag = False
+        for frame_dict in frame_queue:
+            if frame_id == frame_dict['id']:
+                temp_dict = frame_dict
+                if dirty_bits[frame_iter]:
+                    temp_dict['mod'] = 1
+                frame_queue[frame_queue.index(frame_dict)] = temp_dict
+                hit_flag = True
+                break
+
+        if hit_flag:
+            continue
+
+        if len(frame_queue) == frame_size:
+            for frame_dict in frame_queue:
+                if frame_dict['mod'] == 0:
+                    frame_queue.remove(frame_dict)
+                    break
+
+            if len(frame_queue) == frame_size:
+                frame_queue.popleft()
+                write_back += 1
+                interrupt += 1
+
+        temp = dict()
+        temp['id'] = frame_id
+        temp['mod'] = dirty_bits[frame_iter]
+        frame_queue.append(temp)
+
+        page_fault += 1
+        interrupt += 1
+
     return page_fault, interrupt, write_back
 
 
 if __name__ == '__main__':
-    frmae_table = locality_reference()
+    frmae_table = random_reference()
     dirty_bits = random_dirty_bits()
 
     for frame_size in FRAME_list:
         start = time.process_time()
         print("FIFO:", FIFO(frmae_table, dirty_bits, frame_size))
         print("ESC:", enhance_second_chance(frmae_table, dirty_bits, frame_size))
+        print("MYAL:", QQ(frmae_table, dirty_bits, frame_size))
         print("Opt:", Optimal(frmae_table, dirty_bits, frame_size))
         print("Exec time:", time.process_time() - start)
-    
-    # plt.plot(range(1, MAX_STRING), [process_table.count(i) for i in range(1, MAX_STRING)])
-    # plt.show()
-            
 
